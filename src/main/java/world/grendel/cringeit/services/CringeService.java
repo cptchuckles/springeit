26b7ed9e8b6a9eeaf04f9c62065ca174faab8906
Grendel <jcg@grendel.world>
2024-01-31 16:30:43 -0600

    Add CringeService

---
+++ src/main/java/world/grendel/cringeit/services/CringeService.java
@@ -0,0 +1,53 @@
+package world.grendel.cringeit.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import world.grendel.cringeit.models.Cringe;
+import world.grendel.cringeit.models.User;
+import world.grendel.cringeit.repositories.CringeRepository;
+
+/**
+ * CringeService
+ */
+@Service
+public class CringeService {
+    private final CringeRepository cringeRepository;
+
+	public CringeService(CringeRepository cringeRepository) {
+		this.cringeRepository = cringeRepository;
+    }
+
+    public List<Cringe> getAll() {
+        return cringeRepository.findAll();
+    }
+
+    public Cringe getById(Long id) {
+        return cringeRepository.findById(id).orElse(null);
+    }
+
+    public Cringe create(Cringe newCringe, User creator) {
+        newCringe.setUser(creator);
+        return cringeRepository.save(newCringe);
+    }
+
+    public Cringe update(Cringe cringe, User editor) {
+        if (editor.getId() != cringe.getUser().getId() && !editor.isAdmin()) {
+            return null;
+        }
+        return cringeRepository.save(cringe);
+    }
+
+    public boolean deleteById(Long id, User deleter) {
+        Cringe cringe = cringeRepository.findById(id).orElse(null);
+        if (cringe == null) {
+            return false;
+        }
+        if (deleter.getId() != cringe.getUser().getId() && !deleter.isAdmin()) {
+            return false;
+        }
+        cringeRepository.deleteById(id);
+        return true;
+    }
+}
