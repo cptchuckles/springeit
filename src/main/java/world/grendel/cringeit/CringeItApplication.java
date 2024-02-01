6636c063d148a57ca6ec58374d2eb144d58e7489
Grendel <jcg@grendel.world>
2024-02-01 13:51:18 -0600

    Add home redirect to /login

--- src/main/java/world/grendel/cringeit/CringeItApplication.java
+++ src/main/java/world/grendel/cringeit/CringeItApplication.java
@@ -2,12 +2,20 @@ package world.grendel.cringeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class CringeItApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CringeItApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CringeItApplication.class, args);
+    }
+
+    @RequestMapping
+    public String home() {
+        return "redirect:/login";
+    }
 
 }
