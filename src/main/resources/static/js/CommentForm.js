c65056bd400f7d5f1593e7730a2633d92336c5c5
Grendel <jcg@grendel.world>
2024-02-02 00:38:46 -0600

    Make the form work!

--- src/main/resources/static/js/CommentForm.js
+++ src/main/resources/static/js/CommentForm.js
@@ -1,14 +1,15 @@
 import { html, useState, register } from "./deps.js";
 
-function CommentForm({parentCommentId, parentUsername, hiddenElement, cringeId}) {
+function CommentForm({parentComment, hiddenElement, cringeId, closeForm}) {
     const [content, setContent] = useState("");
 
-    const closeForm = () => {
-        this.getParent().appendChild(hiddenElement);
-        this.destroy();
-    };
+    parentComment = parentComment?.props;
 
-    const placeholder = parentUsername ? `Reply to ${parentUsername}` : "Leave a Comment";
+    const placeholder = parentComment
+        ? `Reply to ${parentComment.username}`
+        : "Leave a Comment";
+
+    cringeId ??= parentComment?.cringeId ?? console.error("CommentForm instantiated without a cringeId");
 
     const resetForm = () => {
         setContent("");
@@ -16,39 +17,60 @@ function CommentForm({parentCommentId, parentUsername, hiddenElement, cringeId})
 
     const submitForm = (ev) => {
         ev.preventDefault();
+        const payload = { content };
+        if (parentComment) {
+            payload.parentCommentId = parentComment.commentId;
+        }
+        console.log(payload);
         fetch(`/api/cringe/${cringeId}/comments`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ content }),
+            body: JSON.stringify(payload),
         })
             .then(result => result.json())
             .then(data => {
                 console.log(data);
                 resetForm();
+                if (closeForm) {
+                    closeForm();
+                }
             })
             .catch(e => console.log("can't poast:", e));
     }
 
+    const keydownEventHandler = (ev) => {
+        if (ev.ctrlKey && ev.key === "Enter") {
+            ev.preventDefault();
+            ev.target.form.dispatchEvent(new Event("submit"));
+        }
+        else if (closeForm && ev.key === "Escape") {
+            closeForm();
+        }
+        setContent(ev.target.value);
+    }
+
     return html`
-    <form onSubmit=${submitForm} class="form my-3">
+    <form onSubmit=${submitForm} className=${`form ${parentComment ? "" : "my-3"}`}>
         <textarea
+            autoFocus=${ parentComment ? true : undefined }
+            id=${parentComment ? `reply-${parentComment.commentId}` : undefined }
             name="content"
-            id="content"
             rows="4"
             class="form-control mb-2"
             placeholder="${placeholder}"
             value=${content}
-            onChange=${(ev) => setContent(ev.target.value)}
+            onKeyDown=${keydownEventHandler}
+            onChange=${ev => setContent(ev.target.value)}
         ></textarea>
-        <input type="submit" value="Submit" class="btn btn-success" />
-        ${ parentCommentId && html`
-        <button type="button" onClick=${closeForm} class="btn btn-secondary">Cancel</button>
-        <input type="hidden" name="parentCommentId" value=${parentCommentId} />
-        ` }
+        <p className="d-flex flex-row gap-2">
+            <input type="submit" value="Submit" class="btn btn-success" />
+            ${ parentComment && html`
+            <button type="button" onClick=${closeForm} class="btn btn-secondary">Cancel</button>
+            ` }
+        </p>
     </form>
     `;
 }
 
 register(CommentForm, "comment-form", ["cringeId"]);
-
 export default CommentForm;
